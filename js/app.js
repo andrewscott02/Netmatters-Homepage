3655612021dd35c0d327dafc1297e870ed45fbf7
Andrew Scott <andrewscott02@icloud.com>
2024-04-04 08:40:44 +0100

    Cookies Popup

--- js/app.js
+++ js/app.js
@@ -14,6 +14,23 @@ function GeneralJavaScriptSetup()
 
 //#endregion
 
+//#region Carousel
+
+//$("#Featured").addClass("java-enabled");
+
+//Scales the overlay of the videos to their width so videos cannot be interacted with
+//$(".featured-overlay-container").css("width", $(".featured-overlay-container").parent().css("width"));
+
+$(".slides").slick({
+    //fade:true,
+    autoplay: false,
+    autoplaySpeed: 15000,
+    arrows: false,
+    dots: true
+});
+
+//#endregion
+
 //#region Cookies Popup
 
 var cookiesOpen = false;
@@ -75,22 +92,5 @@ function sendAJAX(){
 }
 
 //#endregion
-
-//#endregion
-
-//#region Carousel
-
-//$("#Featured").addClass("java-enabled");
-
-//Scales the overlay of the videos to their width so videos cannot be interacted with
-//$(".featured-overlay-container").css("width", $(".featured-overlay-container").parent().css("width"));
-
-$(".slides").slick({
-    //fade:true,
-    autoplay: false,
-    autoplaySpeed: 15000,
-    arrows: false,
-    dots: true
-});
 
 //#endregion
