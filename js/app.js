248b2b94fc2db6f6c70f59fcc4873244adb16394
Andrew Scott <andrewscott02@icloud.com>
2024-04-04 08:13:46 +0100

    Cookies Popup -Prevent scrolling when opened -Close menu when accept button is pressed -Menu is closed by default
    -TODO:AJAX requests to see if menu should be opened

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(()=>{
     //Functions here called on page load
     GeneralJavaScriptSetup();
+    CheckCookies();
 });
 
 //#region Enabling/Disabling JavaScript Elements
@@ -10,6 +11,36 @@ function GeneralJavaScriptSetup()
     $(".javascript-disabled").hide();
     $(".requires-javascript").show();
 }
+
+//#endregion
+
+//#region Cookies Popup
+
+var cookiesOpen = false;
+
+function CheckCookies()
+{
+    //Check if cookies menu should be open through AJAX request
+    cookiesOpen ? OpenCookies() : CloseCookies();
+}
+
+function OpenCookies()
+{
+    $('body').addClass('stop-scrolling');
+    $("#CookiesPopup").show();
+}
+
+function CloseCookies()
+{
+    //Send cookies closed via AJAX
+
+    $('body').removeClass('stop-scrolling');
+    $("#CookiesPopup").hide();
+}
+
+$("#AcceptCookies").on("click", ()=>{
+    CloseCookies();
+});
 
 //#endregion
 
