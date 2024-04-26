cff7ad8014630be13783ba1f35222713c20e2746
Andrew Scott <andrewscott02@icloud.com>
2024-04-26 15:16:11 +0100

    Reverted PHP Commit

--- includes/overlay.php
+++
@@ -1,8 +0,0 @@
-<div id="OverlayElements">
-    <div class="manage-consent">
-        <p>Manage Consent</p>
-    </div>
-    <div class="chat">
-        <span class="icon-chat"></span>
-    </div>
-</div>
